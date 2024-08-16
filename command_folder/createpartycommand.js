2ed28876ae1112c8bd32201538b0c451976303b9
JoshLohner <lohnerjosh@gmail.com>
2024-08-16 16:46:38 -0400

    Update createparty command
    Implemented most of button logic

--- command_folder/createpartycommand.js
+++ command_folder/createpartycommand.js
@@ -1,21 +1,30 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { registerButtonHandler } = require('../button_handler');
 
+// Object to store user data for each button
+const buttonClickData = {
+    button1: [],
+    button2: [],
+    button3: [],
+    button4: [],
+    button5: [],
+    button6: [],
+    button7: [],
+    button8: []
+};
+
 module.exports = {
     name: 'createparty',
     async execute(interaction) {
-        // Defer the reply to allow time for processing
         await interaction.deferReply();
 
-        // Extract options from the interaction
         const purpose = interaction.options.getString('purpose');
         const gameplayType = interaction.options.getString('gameplay_type');
         const partySize = interaction.options.getInteger('party_size') || 'Max: 8';
         const description = interaction.options.getString('description') || 'No description provided';
         const requirements = interaction.options.getString('requirements') || 'No requirements specified';
 
-        // Create an embed with the extracted information
-        const embed = new EmbedBuilder()
+        let embed = new EmbedBuilder()
             .setTitle(`Purpose: ${purpose}`)
             .setDescription('Party Finder Created')
             .setColor(0x00AE86)
@@ -27,7 +36,6 @@ module.exports = {
             )
             .setTimestamp();
 
-        // Create two rows of buttons
         const firstButtonRow = new ActionRowBuilder()
             .addComponents(
                 new ButtonBuilder().setCustomId('button1').setLabel('Button 1').setStyle(ButtonStyle.Primary),
@@ -44,39 +52,53 @@ module.exports = {
                 new ButtonBuilder().setCustomId('button8').setLabel('Button 8').setStyle(ButtonStyle.Primary)
             );
 
-        // Register button handlers
-        registerButtonHandler('button1', async (interaction) => {
-            await interaction.reply('Button 1 was clicked!');
-        });
+        const updateEmbedWithUserClicks = () => {
+            // Create a string that lists each user and the button they clicked
+            let userClicks = '';
+            for (const [buttonId, users] of Object.entries(buttonClickData)) {
+                users.forEach(user => {
+                    userClicks += `${user.name} clicked ${buttonId}\n`;
+                });
+            }
 
-        registerButtonHandler('button2', async (interaction) => {
-            await interaction.reply('Button 2 was clicked!');
-        });
+            // Update the embed with the consolidated information
+            embed.spliceFields(4, embed.data.fields.length - 4);
+            embed.addFields({ name: 'User Interactions', value: userClicks || 'No interactions yet', inline: false });
+        };
 
-        registerButtonHandler('button3', async (interaction) => {
-            await interaction.reply('Button 3 was clicked!');
-        });
+        // Register button handlers with user tracking and embed updating
+        const handleButtonClick = async (buttonId, interaction) => {
+            const user = interaction.user;
+            const userIndex = buttonClickData[buttonId].findIndex(u => u.id === user.id);
 
-        registerButtonHandler('button4', async (interaction) => {
-            await interaction.reply('Button 4 was clicked!');
-        });
+            if (userIndex !== -1) {
+                // If the user has already clicked this button, remove them
+                buttonClickData[buttonId].splice(userIndex, 1);
+            } else {
+                // Remove the user from any other button they've clicked
+                for (const [id, users] of Object.entries(buttonClickData)) {
+                    const idx = users.findIndex(u => u.id === user.id);
+                    if (idx !== -1) {
+                        users.splice(idx, 1);
+                    }
+                }
+                // Add the user to the newly clicked button
+                buttonClickData[buttonId].push({ id: user.id, name: user.username });
+            }
 
-        registerButtonHandler('button5', async (interaction) => {
-            await interaction.reply('Button 5 was clicked!');
-        });
+            updateEmbedWithUserClicks();
+            await interaction.update({ embeds: [embed] });
+        };
 
-        registerButtonHandler('button6', async (interaction) => {
-            await interaction.reply('Button 6 was clicked!');
-        });
+        registerButtonHandler('button1', (interaction) => handleButtonClick('button1', interaction));
+        registerButtonHandler('button2', (interaction) => handleButtonClick('button2', interaction));
+        registerButtonHandler('button3', (interaction) => handleButtonClick('button3', interaction));
+        registerButtonHandler('button4', (interaction) => handleButtonClick('button4', interaction));
+        registerButtonHandler('button5', (interaction) => handleButtonClick('button5', interaction));
+        registerButtonHandler('button6', (interaction) => handleButtonClick('button6', interaction));
+        registerButtonHandler('button7', (interaction) => handleButtonClick('button7', interaction));
+        registerButtonHandler('button8', (interaction) => handleButtonClick('button8', interaction));
 
-        registerButtonHandler('button7', async (interaction) => {
-            await interaction.reply('Button 7 was clicked!');
-        });
-
-        registerButtonHandler('button8', async (interaction) => {
-            await interaction.reply('Button 8 was clicked!');
-        });
-        // Edit the reply with the embed and the rows of buttons
         await interaction.editReply({ embeds: [embed], components: [firstButtonRow, secondButtonRow] });
     },
 };
